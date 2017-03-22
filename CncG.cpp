9bf87b031a8f7197de031be17cede83b98dce4d1
wa3kij <wa3kij@gmail.com>
2017-03-22 18:17:26 -0400

    Add files via upload

--- CncG.cpp
+++ CncG.cpp
@@ -8,35 +8,108 @@ class Mach3Splitter
 public:
 	~Mach3Splitter();
 	Mach3Splitter();
+	
+	void Preamble();
+	void EndFile();
+	void ViewMate(char *);  //char *
+	void PreambleVM();
 	char in_file[25],out_file[25],in_line[80],LastZ[80];
 	char out_line[132];
 	double Zetch,Zsafe;
-	FILE *in, *out;
+	FILE *in, *out, *ViewMateFile;
 	int x,iout,getch_count;
-	void Preamble();
-	void EndFile();
 };
 void Mach3Splitter::Preamble()
 {
 	fprintf(out,"G20\n");
-
 	fprintf(out,"G90\n");
-
 	fprintf(out,"G00 Z%6.4f  \n",Zsafe);
-
 	fprintf(out,"M03\n");
-	
 	fprintf(out,"G04 P3.000000\n");
-	
 	fprintf(out,"G01 Z%6.4f F5  \n",Zetch);
-	
 };
+
+void Mach3Splitter::PreambleVM()
+{
+	char xxx;
+	xxx='%';
+	fprintf(ViewMateFile,"G75*\n");
+	fprintf(ViewMateFile,"G70*\n");
+	fprintf(ViewMateFile,"%cFSLAX24Y24*%\n",xxx);
+	fprintf(ViewMateFile,"D10*\n"); 	
+};
+
+
+
+void Mach3Splitter::ViewMate(char *s1)   // char []s1
+{
+	double x,y;
+	int x1,y1,D01=0;
+	int i,ii,ystart=0;
+	char  junk[35];
+	for(i=0;i<25;i++)junk[i]=0X00;
+	
+	junk[0]='X';
+	//if(strncmp(s1,"G00 Z0.4000",11)==0)printf("Start of Block at  ##%s## \n",s1);
+	if(strncmp(s1,"G00 X",5)==0)D01=0;
+	if(strncmp(s1,"G01 X",5)==0)D01=1;
+	if(strncmp(s1,"G01 Z",5)==0)
+	{
+	//	printf("Start of Block at ##%s## \n",s1);
+	}
+	else 
+	
+	if((strncmp(s1,"G01 X",5)==0)||(strncmp(s1,"G00 X",5)==0))
+	{
+		if(s1[4]==junk[0])
+		{         
+			junk[0]='X';
+			ii=1;
+			for(i=5;i<14;i++)
+			{
+				if((s1[i]!=' ')&&(s1[i]!='.'))
+				{
+					junk[ii]=s1[i];
+					ii++;
+					ystart=i+1;
+				}
+			}	
+			junk[ii]='Y';
+			for(i=ystart;i<21;i++)
+			{
+				if((s1[i]!=' ')&&(s1[i]!='.'))
+				{
+					junk[ii]=s1[i];
+					ii++;
+				}
+			}	
+			junk[ii]='D';
+			ii++;
+			junk[ii]='0';
+			ii++;
+			junk[ii]='0';
+			if(D01>0)junk[ii]='1';
+			ii++;
+			junk[ii]='*';
+			
+			// G01 X1.7632  Y1.3551 
+			// G01 X27.9633 Y13.2752 
+			// 012345678901234567892
+			//            1111111112  
+			
+		}
+		fprintf(ViewMateFile,"%s\n",junk);		
+	}
+	s1[0]=0x00;
+}
+
 void Mach3Splitter::EndFile()
 {
 	fprintf(out,"G01 Z%6.4f F5   \n",Zsafe);
 	fprintf(out,"M05\n");
 	fprintf(out,"M02\n");
 };
+
 Mach3Splitter::Mach3Splitter ()
 {
 	in_file[0]=0x00;
@@ -45,12 +118,11 @@ Mach3Splitter::Mach3Splitter ()
 	out_line[0]=0x00;
 	iout=0;
 	getch_count=0;
-	cout <<"Constructor \n";
+//	cout <<"Constructor \n";
 };
 Mach3Splitter::~Mach3Splitter ()
 {
-
-	cout <<"Destructor \n";
+//	cout <<"Destructor \n";
 	fclose(in);
 	fprintf(out,"}\n");
 	fclose(out);
@@ -58,47 +130,51 @@ Mach3Splitter::~Mach3Splitter ()
 
 
 
+
 int main()
 {
 	int lineCount = 0;
 	int File_Count = 1;
-	Mach3Splitter Help;
-	Help.Zetch = 0.007;
-	Help.Zsafe = 0.7500;
+	Mach3Splitter PCB;
+	PCB.Zetch = 0.007;
+	PCB.Zsafe = 0.7500;
 	
 	printf(" Please enter the etch depth, -0.007 would be 7 mils etch down.\n");
-	cin >> Help.Zetch;
+	cin >> PCB.Zetch;
 	printf(" Please enter the Z safe height 0.75 would be 750 mills above your Z 0.0.\n");
-	cin >> Help.Zsafe;
+	cin >> PCB.Zsafe;
 	printf(" Please enter the input file name.\n");
-	cin >> Help.in_file;
-	printf("%s infile %s outfile \n",Help.in_file,Help.out_file);
-	sprintf(Help.out_file,"Mill%d.tap",File_Count);
-	Help.in = fopen(Help.in_file,"r");
-	Help.out = fopen(Help.out_file,"w");
-	
-	
-	while(!feof(Help.in))
+	cin >> PCB.in_file;
+	//   printf("%s infile %s outfile \n",PCB.in_file,PCB.out_file);
+	sprintf(PCB.out_file,"Mill%d.tap",File_Count);
+	PCB.in = fopen(PCB.in_file,"r");
+	PCB.out = fopen(PCB.out_file,"w");
+	PCB.ViewMateFile = fopen("View.Mate","w");
+	PCB.PreambleVM();
+	while(!feof(PCB.in))
 	{
 		lineCount++;
-		fgets(Help.in_line,80,Help.in);
-		fprintf(Help.out,"%s",Help.in_line);
+		fgets(PCB.in_line,80,PCB.in);
+		
+		PCB.ViewMate(PCB.in_line);
+		
+		fprintf(PCB.out,"%s",PCB.in_line);
 		//     G00 Z0
-		Help.LastZ[0]=0x00;
-		if (strncmp (Help.in_line,"G00 Z0",6) == 0)
-		strcat(Help.LastZ,Help.in_line);
+		PCB.LastZ[0]=0x00;
+		if (strncmp (PCB.in_line,"G00 Z0",6) == 0)
+		strcat(PCB.LastZ,PCB.in_line);
 		if(lineCount>480)
 		{
-			Help.EndFile();
+			PCB.EndFile();
 			lineCount=0;
 			File_Count++;
-			Help.out_file[0]=0x00;
-			sprintf(Help.out_file,"Mill%d.tap",File_Count);
-			fclose(Help.out);
-			Help.out = fopen(Help.out_file,"w");
+			PCB.out_file[0]=0x00;
+			sprintf(PCB.out_file,"Mill%d.tap",File_Count);
+			fclose(PCB.out);
+			PCB.out = fopen(PCB.out_file,"w");
 			
-			Help.Preamble();
-			fprintf(Help.out,Help.LastZ);
+			PCB.Preamble();
+			fprintf(PCB.out,PCB.LastZ);
 		}
 	}
 
